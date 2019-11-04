95dca8e52e35770526f7e21defaccea54bb68c09
Unknown <unknown@example.com>
2019-11-04 21:19:49 +0100

    seguir

--- app/src/main/java/com/example/luisreyes/proyecto_aguas/Screen_Filter_Tareas.java
+++ app/src/main/java/com/example/luisreyes/proyecto_aguas/Screen_Filter_Tareas.java
@@ -2,15 +2,20 @@ package com.example.luisreyes.proyecto_aguas;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -18,6 +23,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * Created by luis.reyes on 04/11/2019.
@@ -46,9 +52,15 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
             ,layout_filtro_datos_privados_screen_advance_filter
             , layout_filtro_tipo_tarea_screen_advance_filter
             ,layout_filtro_datos_unicos_screen_advance_filter,
-            layout_filtro_direccion_checkboxes_screen_advance_filter;
+            layout_filtro_checkboxes_screen_advance_filter,
+            layout_filtro_accept_filter_screen_advance_filter;
 
     private ListView listView_contadores_screen_advance_filter;
+
+    private Button button_filtrar;
+    private HashMap<String, String> mapaTiposDeTarea;
+
+    private TextView textView_checkboxes_type;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +72,21 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
         lista_desplegable.add("DATOS PRIVADOS");
         lista_desplegable.add("TIPO DE TAREA");
         lista_desplegable.add("DATOS ÚNICOS");
+
+        mapaTiposDeTarea = new HashMap<>();
+        mapaTiposDeTarea.put("", "NUEVO CONTADOR INSTALAR");
+        mapaTiposDeTarea.put("NCI", "NUEVO CONTADOR INSTALAR");
+        mapaTiposDeTarea.put("U", "USADO CONTADOR INSTALAR");
+        mapaTiposDeTarea.put("T", "BAJA O CORTE DE SUMINISTRO");
+        mapaTiposDeTarea.put("TBDN", "BAJA O CORTE DE SUMINISTRO");
+        mapaTiposDeTarea.put("LFTD", "LIMPIEZA DE FILTRO Y TOMA DE DATOS");
+        mapaTiposDeTarea.put("D", "DATOS");
+        mapaTiposDeTarea.put("TD", "TOMA DE DATOS");
+        mapaTiposDeTarea.put("I", "INSPECCIÓN");
+        mapaTiposDeTarea.put("CF", "COMPROBAR EMISOR");
+        mapaTiposDeTarea.put("EL", "EMISOR LECTURA");
+        mapaTiposDeTarea.put("SI", "SOLO INSTALAR");
+        mapaTiposDeTarea.put("R", "REFORMA MAS CONTADOR");
 
         listView_contadores_screen_advance_filter = (ListView)findViewById(R.id.listView_contadores_screen_advance_filter);
         spinner_filtro_tareas = (Spinner) findViewById(R.id.spinner_tipo_filtro_screen_advance_filter);
@@ -74,14 +101,45 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
         spinner_filtro_serie_screen_advance_filter = (Spinner) findViewById(R.id.spinner_filtro_serie_screen_advance_filter);
 
         layout_filtro_direccion_screen_advance_filter      = (LinearLayout) findViewById(R.id.layout_filtro_direccion_screen_advance_filter);
-        layout_filtro_direccion_checkboxes_screen_advance_filter = (LinearLayout) findViewById(R.id.layout_filtro_direccion_checkboxes_screen_advance_filter);
+        layout_filtro_checkboxes_screen_advance_filter = (LinearLayout) findViewById(R.id.layout_filtro_checkboxes_screen_advance_filter);
         layout_filtro_datos_privados_screen_advance_filter = (LinearLayout) findViewById(R.id.layout_filtro_datos_privados_screen_advance_filter);
         layout_filtro_tipo_tarea_screen_advance_filter     = (LinearLayout) findViewById(R.id.layout_filtro_tipo_tarea_screen_advance_filter);
         layout_filtro_datos_unicos_screen_advance_filter   = (LinearLayout) findViewById(R.id.layout_filtro_datos_unicos_screen_advance_filter);
+        layout_filtro_accept_filter_screen_advance_filter  = (LinearLayout) findViewById(R.id.layout_filtro_accept_filter_screen_advance_filter);
+
+        textView_checkboxes_type = (TextView) findViewById(R.id.textView_checkboxes_type);
+        button_filtrar = (Button) findViewById(R.id.button_fitrar_screen_filter_tareas);
 
         ArrayAdapter arrayAdapter_spinner = new ArrayAdapter(this, android.R.layout.simple_spinner_item, lista_desplegable);
         spinner_filtro_tareas.setAdapter(arrayAdapter_spinner);
 
+        button_filtrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Screen_Login_Activity.playOnOffSound(getApplicationContext());
+                final Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+                // Use bounce interpolator with amplitude 0.2 and frequency 20
+                MyBounceInterpolator interpolator = new MyBounceInterpolator(
+                        MainActivity.AMPLITUD_BOUNCE, MainActivity.FRECUENCY_BOUNCE);
+                myAnim.setInterpolator(interpolator);
+                myAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation arg0) {
+                        // TODO Auto-generated method stub
+//                Toast.makeText(context,"Animacion iniciada", Toast.LENGTH_LONG).show();
+                    }
+                    @Override
+                    public void onAnimationRepeat(Animation arg0) {
+                        // TODO Auto-generated method stub
+                    }
+                    @Override
+                    public void onAnimationEnd(Animation arg0) {
+
+                    }
+                });
+                button_filtrar.startAnimation(myAnim);
+            }
+        });
 
         spinner_filtro_poblacion_screen_advance_filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -104,16 +162,36 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
 
                 if(current_action_listview == SELECTING_CALLES){
 //                    openMessage("Elegido", object_click);
-                    if(((LinearLayout) layout_filtro_direccion_checkboxes_screen_advance_filter).getChildCount() > 0)
-                        ((LinearLayout) layout_filtro_direccion_checkboxes_screen_advance_filter).removeAllViews();
+                    if(((LinearLayout) layout_filtro_checkboxes_screen_advance_filter).getChildCount() > 0)
+                        ((LinearLayout) layout_filtro_checkboxes_screen_advance_filter).removeAllViews();
+                    layout_filtro_accept_filter_screen_advance_filter.setVisibility(View.VISIBLE);
+                    textView_checkboxes_type.setText("PORTALES");
                     fillCheckBoxesWithPortales(spinner_filtro_poblacion_screen_advance_filter
                             .getSelectedItem().toString(), object_click);
 
                     listView_contadores_screen_advance_filter.setVisibility(View.GONE);
                 }
-
             }
         });
+
+        spinner_filtro_tipo_tarea_screen_advance_filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selected = spinner_filtro_tipo_tarea_screen_advance_filter
+                        .getAdapter().getItem(i).toString();
+                if(!selected.isEmpty() && selected!=null && !selected.equals("Ninguno")) {
+                    if(((LinearLayout) layout_filtro_checkboxes_screen_advance_filter).getChildCount() > 0)
+                        ((LinearLayout) layout_filtro_checkboxes_screen_advance_filter).removeAllViews();
+                    layout_filtro_accept_filter_screen_advance_filter.setVisibility(View.VISIBLE);
+                    textView_checkboxes_type.setText("CALIBRES");
+                    fillCheckBoxesWithCalibres(selected);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
         spinner_filtro_tareas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -134,9 +212,9 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
 //                    lista_filtro_actual = lista_filtro_Tareas;
 //                    arrayAdapter = new ArrayAdapter(Screen_Advance_Filter.this, R.layout.list_text_view, lista_filtro_Tareas);
 //                    listView_contadores_screen_advance_filter.setAdapter(arrayAdapter);
-//                    hideAllFilters();
-//                    layout_filtro_tipo_tarea_screen_advance_filter.setVisibility(View.VISIBLE);
-//                    fillFilterTiposTareas();
+                    hideAllFilters();
+                    layout_filtro_tipo_tarea_screen_advance_filter.setVisibility(View.VISIBLE);
+                    fillFilterTiposTareas();
                 }
                 else if(lista_desplegable.get(i).contains("DATOS ÚNICOS")){
 //                    lista_filtro_actual = lista_filtro_numero_serie;
@@ -178,11 +256,14 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
                 if(!poblacion.equals("null") && !poblacion.isEmpty() && !calle.equals("null") && !calle.isEmpty()){
                     if(poblacion.equals(poblacion_item.trim()) && calle.equals(calle_item.trim()) ) {
                         String numero_portal = jsonObject.getString(DBtareasController.numero).trim();
-                        lista_portales.add(numero_portal);
-                        CheckBox cb = new CheckBox(getApplicationContext());
-                        cb.setText(numero_portal);
-                        layout_filtro_direccion_checkboxes_screen_advance_filter.addView(cb);
+                        if(!lista_portales.contains(numero_portal)) {
+                            lista_portales.add(numero_portal);
+                            CheckBox cb = new CheckBox(getApplicationContext());
+                            cb.setText(numero_portal);
+                            cb.setLayoutDirection(View.LAYOUT_DIRECTION_INHERIT);
+                            layout_filtro_checkboxes_screen_advance_filter.addView(cb);
 //                            lista_ordenada_de_tareas.add(Screen_Table_Team.orderTareaFromJSON(jsonObject));
+                        }
                     }
                 }
             } catch (JSONException e) {
@@ -193,6 +274,7 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
     }
 
     private void fillListViewWithCalles(String poblacion_item) {
+        listView_contadores_screen_advance_filter.setVisibility(View.VISIBLE);
         ArrayList<String> lista_desplegable = new ArrayList<String>();
 //        lista_ordenada_de_tareas.clear();
         for (int i = 1; i <= team_or_personal_task_selection_screen_Activity.dBtareasController.countTableTareas(); i++) {
@@ -216,18 +298,99 @@ public class Screen_Filter_Tareas extends AppCompatActivity{
             }
         }
         Collections.sort(lista_desplegable, String.CASE_INSENSITIVE_ORDER);
-        ArrayAdapter arrayAdapter_listView = new ArrayAdapter(this, android.R.layout.simple_spinner_item, lista_desplegable);
+        ArrayAdapter arrayAdapter_listView = new ArrayAdapter(this,  R.layout.list_text_view, lista_desplegable);
         listView_contadores_screen_advance_filter.setAdapter(arrayAdapter_listView);
     }
 
-
     public void hideAllFilters(){
+        listView_contadores_screen_advance_filter.setVisibility(View.GONE);
+        layout_filtro_accept_filter_screen_advance_filter.setVisibility(View.GONE);
         layout_filtro_direccion_screen_advance_filter.setVisibility(View.GONE);
         layout_filtro_datos_privados_screen_advance_filter.setVisibility(View.GONE);
         layout_filtro_tipo_tarea_screen_advance_filter.setVisibility(View.GONE);
         layout_filtro_datos_unicos_screen_advance_filter.setVisibility(View.GONE);
     }
 
+    private void fillCheckBoxesWithCalibres(String tipo_tarea_item) {
+        ArrayList<String> lista_desplegable = new ArrayList<String>();
+        for (int i = 1; i <= team_or_personal_task_selection_screen_Activity.
+                dBtareasController.countTableTareas(); i++) {
+            try {
+                JSONObject jsonObject = new JSONObject(team_or_personal_task_selection_screen_Activity.
+                        dBtareasController.get_one_tarea_from_Database(i));
+                String tipo_tarea = jsonObject.getString(DBtareasController.tipo_tarea).trim();
+                String calibre = jsonObject.getString(DBtareasController.calibre_toma).trim();
+                if((tipo_tarea.equals("null") && calibre.isEmpty()) || (tipo_tarea.isEmpty()
+                        && calibre.isEmpty()) || (tipo_tarea.isEmpty() && calibre.equals("null"))
+                        || (tipo_tarea.equals("null") && calibre.equals("null"))  ){
+                }else {
+                    String tipo = "";
+                    if(mapaTiposDeTarea.containsKey(tipo_tarea)) {
+                        tipo = mapaTiposDeTarea.get(tipo_tarea);
+                        if (tipo.equals(tipo_tarea_item)) {
+                            if (!lista_desplegable.contains(calibre)) {
+                                lista_desplegable.add(calibre);
+                                CheckBox cb = new CheckBox(getApplicationContext());
+                                cb.setText(calibre);
+                                layout_filtro_checkboxes_screen_advance_filter.addView(cb);
+                            }
+                        }
+                    }else if(tipo_tarea.contains("T") && tipo_tarea.contains("\"")){
+                        tipo = "BAJA O CORTE DE SUMINISTRO";
+                        if (tipo.equals(tipo_tarea_item)) {
+                            if (!lista_desplegable.contains(calibre)) {
+                                lista_desplegable.add(calibre);
+                                CheckBox cb = new CheckBox(getApplicationContext());
+                                cb.setText(calibre);
+                                layout_filtro_checkboxes_screen_advance_filter.addView(cb);
+                            }
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void fillFilterTiposTareas() {
+        ArrayList<String> lista_desplegable = new ArrayList<String>();
+//        lista_ordenada_de_tareas.clear();
+        for (int i = 1; i <= team_or_personal_task_selection_screen_Activity.dBtareasController.countTableTareas(); i++) {
+            try {
+                JSONObject jsonObject = new JSONObject(team_or_personal_task_selection_screen_Activity.
+                        dBtareasController.get_one_tarea_from_Database(i));
+                String tipo_tarea = jsonObject.getString(DBtareasController.tipo_tarea).trim();
+                String calibre = jsonObject.getString(DBtareasController.calibre_toma).trim();
+                if ((tipo_tarea.equals("null") && calibre.isEmpty()) || (tipo_tarea.isEmpty() && calibre.isEmpty())
+                        || (tipo_tarea.isEmpty() && calibre.equals("null")) || (tipo_tarea.equals("null") && calibre.equals("null"))) {
+                } else {
+                    String tipo = "";
+                    if (mapaTiposDeTarea.containsKey(tipo_tarea)) {
+                        tipo = mapaTiposDeTarea.get(tipo_tarea);
+                        if (tipo != null && !tipo.isEmpty() && !tipo.equals("null")) {
+                            if (!lista_desplegable.contains(tipo)) {
+                                lista_desplegable.add(tipo);
+                            }
+//                            lista_ordenada_de_tareas.add(Screen_Table_Team.orderTareaFromJSON(jsonObject));
+                        }
+                    } else if (tipo_tarea.contains("T") && tipo_tarea.contains("\"")) {
+                        tipo = "BAJA O CORTE DE SUMINISTRO";
+                        if (!lista_desplegable.contains(tipo)) {
+                            lista_desplegable.add(tipo);
+                        }
+//                        lista_ordenada_de_tareas.add(Screen_Table_Team.orderTareaFromJSON(jsonObject));
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Collections.sort(lista_desplegable, String.CASE_INSENSITIVE_ORDER);
+        lista_desplegable.add(0,"Ninguno");
+        ArrayAdapter arrayAdapter_spinner = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista_desplegable);
+        spinner_filtro_tipo_tarea_screen_advance_filter.setAdapter(arrayAdapter_spinner);
+    }
     public void fillFilterPoblacion(){
         current_action_listview = SELECTING_CALLES;
         ArrayList<String> lista_desplegable = new ArrayList<String>();
